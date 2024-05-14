c1cc776374524a8c65bc09643e3cad0a9ba42a24
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-05-14 15:51:28 -0400

    stringsLongerThan function complete

--- app.js
+++ app.js
@@ -32,3 +32,18 @@ function longestString(array) {
   return longest;
 }
 console.log(longestString(["say", "hello", "in", "the", "morning"]));
+
+//Tkae an array of strings, and a number and return an array of the strings that aew longer than the number given
+function stringsLongerThan(array, num) {
+  let newArr = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].length > num) {
+      newArr.push(array[i]);
+    }
+  }
+
+  return newArr;
+}
+
+console.log(stringsLongerThan(["say", "hello", "in", "the", "morning"], 3));
