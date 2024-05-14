b51def7f5410d4a55c55d1e5fe182de16288e3c7
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-05-14 15:38:03 -0400

    Sum of values in an array complete

---
+++ app.js
@@ -0,0 +1,13 @@
+//Part 1
+
+//Take an array of numbers and return the sum
+function sumOfArray(array) {
+  let sum = 0;
+  array.forEach((element) => {
+    sum += element;
+  });
+
+  return sum;
+}
+
+console.log(sumOfArray([1, 2, 3, 4, 5, 6]));
